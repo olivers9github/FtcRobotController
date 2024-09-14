215022e77c2f0b16dfc39f32109d0bdb9de04b0f
olivers9github <olivers9@nwfsc.edu>
2024-09-14 17:50:42 -0500

    IntoTheDeepArm.java renovation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepArm.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -9,7 +8,31 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class IntoTheDeepArm extends LinearOpMode {
     private DcMotor extendArm;
     private DcMotor rotateArm;
-
+/* Define motors in DriverHub */
     @Override
-    public void runOpMode()
-}
+    public void runOpMode() {
+        extendArm = hardwareMap.get(DcMotor.class,"extendArm");
+        rotateArm = hardwareMap.get(DcMotor.class,"rotateArm");
+        waitForStart();
+        while( opModeIsActive()){
+            double max;
+            double extend = gamepad2.right_trigger;
+            double retract = -gamepad2.left_trigger;
+            boolean cwRotate = gamepad2.dpad_right;
+            boolean ccRotate = gamepad2.dpad_left;
+            /* cw = clockwise cc = counterclockwise */
+            double extArmPower = extend + retract;
+            if (cwRotate == true) {
+                rotateArm.setPower(1);
+            }
+            else if (ccRotate == true) {
+                rotateArm.setPower(-1);
+            }
+            extendArm.setPower(extArmPower);
+
+
+
+            }
+        }
+    }
+
