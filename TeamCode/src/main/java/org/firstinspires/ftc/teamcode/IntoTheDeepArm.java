90bffb11a098f7051c17a09b3c3f2932eebe8702
olivers9github <olivers9@nwfsc.edu>
2024-09-14 16:34:30 -0500

    IntoTheDeepArm Commit
    Explains itself

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepArm.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="HighFiveTest", group="TestCode")
+public class IntoTheDeepArm extends LinearOpMode {
+    private DcMotor extendArm;
+    private DcMotor rotateArm;
+
+    @Override
+    public void runOpMode()
+}
